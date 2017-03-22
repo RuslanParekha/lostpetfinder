5e95c8a4e12038f18ecaefa3ed55df4ee8711015
ruslan <rakitor@gmail.com>
2017-03-22 16:17:16 +0200

    Ready to deploy -void  setIncedentRepository & setUserRepository

--- src/java/com/com/services/UserDetailsServiceImpl.java
+++ src/java/com/com/services/UserDetailsServiceImpl.java
@@ -36,4 +36,8 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
         return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),grantedAuthorities);
     }
+
+    public void setUserRepository(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
 }
