c30191cb8cf35d81c4f8fa12217ccd54ab020956
ruslan <rakitor@gmail.com>
2017-03-23 17:27:11 +0200

    Ready to deploy

--- src/java/com/com/services/IncedentService.java
+++ src/java/com/com/services/IncedentService.java
@@ -2,7 +2,7 @@ package com.com.services;
 
 import com.com.model.Incedent;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+
 
 import java.util.List;
 
